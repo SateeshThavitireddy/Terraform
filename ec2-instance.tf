f33b787075e7f7d297fced9de77a2efbd86792d7
sateesh thavitireddy <sateesh.thavitireddy@gmail.com>
2025-12-23 15:06:37 +0000

    terraform

--- ec2-instance.tf
+++ ec2-instance.tf
@@ -1,8 +1,31 @@
 resource "aws_instance" "Terraform" {
-  ami = "ami-09c813fb71547fc4f"
-  instance_type = "t3.micro"
+  ami = var.ami_id
+  instance_type = var.instance_type
+  vpc_security_group_ids = [aws_security_group.allow-for-terraform.id]
+
+  tags = var.ec2_tags
+}
+
+resource "aws_security_group" "allow-for-terraform" {
+  name        = "allow-for-terraform"
+
+  ingress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+    egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
 
   tags = {
-    Name = "Terraform server"
+    Name = "allow-for-terraform"
   }
+  
 }
---
+++ varibles.tf
@@ -0,0 +1,13 @@
+variable "ami_id"{
+    type = string
+    default ="ami-09c813fb71547fc4f"
+}
+variable "instance_type"{
+    type =string 
+    default = "t3.micro"
+}
+variable "ec2_tags"{
+    type= map
+    Name = "Terraform server"
+    Terraform = "true"
+}
